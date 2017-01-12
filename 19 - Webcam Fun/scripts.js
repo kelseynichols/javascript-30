77e4272887bf5e280957a456cd17812d5c9b7d92
Kelsey Nichols <kelseynichols2@gmail.com>
2017-01-12 11:27:51 -0600

    rgb slit working

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -26,7 +26,8 @@ function paintToCanvas() {
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
         ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
@@ -64,32 +65,32 @@ function rgbSplit(pixels) {
   return pixels;
 }
 
-function greenScreen(pixels) {
-  const levels = {};
+// function greenScreen(pixels) {
+//   const levels = {};
 
-  document.querySelectorAll('.rgb input').forEach((input) => {
-    levels[input.name] = input.value;
-  });
+//   document.querySelectorAll('.rgb input').forEach((input) => {
+//     levels[input.name] = input.value;
+//   });
 
-  for (i = 0; i < pixels.data.length; i = i + 4) {
-    red = pixels.data[i + 0];
-    green = pixels.data[i + 1];
-    blue = pixels.data[i + 2];
-    alpha = pixels.data[i + 3];
+//   for (i = 0; i < pixels.data.length; i = i + 4) {
+//     red = pixels.data[i + 0];
+//     green = pixels.data[i + 1];
+//     blue = pixels.data[i + 2];
+//     alpha = pixels.data[i + 3];
 
-    if (red >= levels.rmin
-      && green >= levels.gmin
-      && blue >= levels.bmin
-      && red <= levels.rmax
-      && green <= levels.gmax
-      && blue <= levels.bmax) {
-      // take it out!
-      pixels.data[i + 3] = 0;
-    }
-  }
+//     if (red >= levels.rmin
+//       && green >= levels.gmin
+//       && blue >= levels.bmin
+//       && red <= levels.rmax
+//       && green <= levels.gmax
+//       && blue <= levels.bmax) {
+//       // take it out!
+//       pixels.data[i + 3] = 0;
+//     }
+//   }
 
-  return pixels;
-}
+//   return pixels;
+// }
 
 
 
